7d955ba2c47707a6db12c60837c32c31ae8d97e7
salman_khosravipour <khosravipour.salman@gmail.com>
2022-06-28 15:54:17 +0430

    remove a newline

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
+	
 
 browser = webdriver.Chrome()
 browser.get('https://www.instagram.com/')
@@ -13,4 +13,3 @@ password = browser.find_element(by=By.NAME, value='password')
 password.send_keys('passwd@2')
 
 submit_btn = browser.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[3]/button').click()
-
