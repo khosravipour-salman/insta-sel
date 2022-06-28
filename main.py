01841e2f85680bed544c651acc0966d377f9838f
salman_khosravipour <khosravipour.salman@gmail.com>
2022-06-28 15:45:08 +0430

    login into insta

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.chrome()
+
+browser = webdriver.Chrome()
+browser.get('https://www.instagram.com/')
+
+username = browser.find_element(by=By.NAME, value='username')
+username.send_keys('thisistestingselenium@gmail.com')
+
+password = browser.find_element(by=By.NAME, value='password')
+password.send_keys('passwd@2')
+
+submit_btn = browser.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[3]/button').click()
+
